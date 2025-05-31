d14bb440748a421fbf0df52a9cf88a3d652280d4
Kwesi Ifeogwu <kwesitech23@yahoo.com>
2025-05-31 05:48:41 -0400

    bashscript mysql

--- MySQL/install _mysql.sh
+++ MySQL/install _mysql.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 DATABASE_PASS='admin123'
+
+# Update OS and install dependencies
 sudo yum update -y
 sudo yum install epel-release -y
 sudo yum install git zip unzip -y
 sudo yum install mariadb-server -y
 
-
-# starting & enabling mariadb-server
+# Start and enable MariaDB service
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
+
+# Clone the repo containing the database dump
 cd /tmp/
 git clone -b local-setup https://github.com/KwesiLovesTech/Vprofile-Project-Multi-Tier-Web-Application-Stack-Setup-Locally.git
-#restore the dump file for the application
+
+# Secure MariaDB installation and set password
 sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
-sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+# Create the application database and user
 sudo mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
 sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 
-# Restart mariadb-server
+# Restart the database service
 sudo systemctl restart mariadb
 
-
-#starting the firewall and allowing the mariadb to access from port no. 3306
+# Configure the firewall for external DB access
 sudo systemctl start firewalld
 sudo systemctl enable firewalld
 sudo firewall-cmd --get-active-zones
--- RabbitMq/install_rabbitmq.sh
+++ RabbitMq/install_rabbitmq.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+# Update and install dependencies
 sudo yum install epel-release -y
 sudo yum update -y
 sudo yum install wget -y
+
+# Install Erlang (required by RabbitMQ)
 cd /tmp/
 wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
 sudo rpm -Uvh erlang-solutions-2.0-1.noarch.rpm
 sudo yum -y install erlang socat
+
+# Add RabbitMQ repository and install
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
 sudo yum install rabbitmq-server -y
+
+# Start and enable the RabbitMQ service
 sudo systemctl start rabbitmq-server
 sudo systemctl enable rabbitmq-server
 sudo systemctl status rabbitmq-server
+
+# Allow external access to RabbitMQ web console
 sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
+
+# Create test admin user
 sudo rabbitmqctl add_user test test
 sudo rabbitmqctl set_user_tags test administrator
+
+# Restart RabbitMQ to apply changes
 sudo systemctl restart rabbitmq-server
-```
